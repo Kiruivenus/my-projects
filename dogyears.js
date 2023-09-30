18e8a43d8f494485a3462a1e5984b3d4c9bc4cc5
Kiruivenus <139698673+Kiruivenus@users.noreply.github.com>
2023-09-30 16:39:41 +0300

    converts human years to dog years

---
+++ dogyears.js
@@ -0,0 +1,15 @@
+let myAge=21;
+
+//create a variable
+const earlyYears=2*10.5;
+let laterYears=(myAge-2)*4;
+let myAgeInDogYears = earlyYears + laterYears;
+let myName="kiruivenus";
+myName=myName.toLowerCase();
+console.log(` my name is ${myName}. iam ${myAge} in human age which is ${myAgeInDogYears} old in dog years`);
+
+
+
+
+
+//C:\html\dogyears.js
